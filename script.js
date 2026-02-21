a685c3e611bcda2dfbbcf01e666338680cd67628
lizigigauri01 <lizi.gigaurite01@geolab.edu.ge>
2026-02-21 17:37:29 +0400

    day1

---
+++ script.js
@@ -0,0 +1,33 @@
+let slideIndex = 0;
+let slides = document.getElementsByClassName("carousel-slide");
+let dots = document.getElementsByClassName("dot");
+
+function showSlide(n) {
+  if (n >= slides.length) slideIndex = 0;
+  if (n < 0) slideIndex = slides.length - 1;
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = 0; 
+  }
+
+  slides[slideIndex].style.opacity = 1; 
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  dots[slideIndex].className += " active";
+}
+
+function changeSlide(n) {
+  slideIndex += n;
+  if (slideIndex >= slides.length) slideIndex = 0;
+  if (slideIndex < 0) slideIndex = slides.length - 1;
+  showSlide(slideIndex);
+}
+
+function currentSlide(n) {
+  slideIndex = n;
+  showSlide(slideIndex);
+}
+
+showSlide(slideIndex);
